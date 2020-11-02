363c5b2ef736d58fa35ea974b405a478ca50ea3e
kanchan-shinde <65505512+kanchan-shinde@users.noreply.github.com>
2020-11-02 15:13:13 +0530

    Add files via upload

---
+++ assig-issue/assignment.issues.js
@@ -0,0 +1,45 @@
+function showName() {
+    var name = prompt("Enter your name");
+    document.getElementById('uname').innerHTML = "name";
+}
+
+function calculateNum(num_1, num_2, calc_type) {
+    /** Calculate two numbers as per calulation type, and show result in the text box */
+    var result;
+    switch (calc_type) {
+        case 'add':
+            result = Number(num_1) + Number(num_2);
+        case 'minus':
+            result = Number(num_1) - Number(num_2);
+        case 'mul':
+            result = Number(num_1) * Number(num_2);
+        case 'div':
+            result = Number(num_1) / Number(num_2);
+        case 'mod':
+            result = Number(num_1) % Number(num_2);
+    }
+
+    document.getElementById('output_box').value = result;
+    console.log(result);
+}
+
+function ChangeTitle() {
+    var all_titles = document.getElementsByClassName('title-text');
+
+    for( var val of all_titles){
+    val.classList.remove('text-secondary');
+    val.innerHTML = "Successfully Changed titles";
+
+}
+
+
+function printPage() {
+    var choice = confirm("Do you want to print?");
+    if (choice) {
+        window.print();
+    }
+}
+
+function greetUser() {
+    alert ( "Hello and Welcome to our website");
+}
